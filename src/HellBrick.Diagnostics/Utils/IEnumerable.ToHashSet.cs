29072fa2738757e70b412b4af7272af0655c5aaa
HellBrick <hellbrick@gmail.com>
2015-07-25 12:03:35 +0300

    Added IEnumerable.ToHashSet() extension

---
+++ src/HellBrick.Diagnostics/Utils/IEnumerable.ToHashSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellBrick.Diagnostics.Utils
+{
+	public static partial class EnumerableExtensions
+	{
+		public static HashSet<T> ToHashSet<T>( this IEnumerable<T> sequence )
+		{
+			return new HashSet<T>( sequence );
+		}
+	}
+}
