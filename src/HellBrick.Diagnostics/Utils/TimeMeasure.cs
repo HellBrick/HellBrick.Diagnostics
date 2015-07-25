fa8ce9dcd7609bddc00edcd992af9c8f79feb1b4
HellBrick <hellbrick@gmail.com>
2015-07-25 12:10:13 +0300

    Added TimeMeasure

---
+++ src/HellBrick.Diagnostics/Utils/TimeMeasure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellBrick.Diagnostics.Utils
+{
+	public class TimeMeasure : IDisposable
+	{
+		private readonly string _activityName;
+		private readonly Stopwatch _stopwatch;
+		private readonly Action<string, TimeSpan> _onMeasured;
+
+		private TimeMeasure( string activityName, Action<string, TimeSpan> onMeasured )
+		{
+			_activityName = activityName;
+			_onMeasured = onMeasured;
+			_stopwatch = Stopwatch.StartNew();
+		}
+
+		public static TimeMeasure ToDebug( string activityName )
+		{
+			return new TimeMeasure( activityName, ( activity, time ) => Debug.WriteLine( $"{time} - {activity}" ) );
+      }
+
+		public void Dispose()
+		{
+			_stopwatch.Stop();
+			_onMeasured( _activityName, _stopwatch.Elapsed );
+		}
+	}
+}
