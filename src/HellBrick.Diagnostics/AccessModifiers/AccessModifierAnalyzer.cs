858eb40abacaba33d8f3a99127c063d21844f490
HellBrick <hellbrick@gmail.com>
2015-09-23 21:40:06 +0300

    Implemented a VisibilityModifier prototype

---
+++ src/HellBrick.Diagnostics/AccessModifiers/AccessModifierAnalyzer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+
+namespace HellBrick.Diagnostics.AccessModifiers
+{
+	[DiagnosticAnalyzer( LanguageNames.CSharp )]
+	public class AccessModifierAnalyzer : DiagnosticAnalyzer
+	{
+		public const string DiagnosticID = IDPrefix.Value + "MissingAccessModifier";
+
+		private const string _title = "Access modifier is missing";
+		private const string _category = DiagnosticCategory.Style;
+
+		private static readonly DiagnosticDescriptor _rule = new DiagnosticDescriptor( DiagnosticID, _title, _title, _category, DiagnosticSeverity.Warning, isEnabledByDefault: true );
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create( _rule );
+
+		public override void Initialize( AnalysisContext context )
+		{
+			context.RegisterSyntaxNodeAction( FindMissingVisibilityModifiers, DeclarationHandlers.SupportedSyntaxKinds );
+		}
+
+		private void FindMissingVisibilityModifiers( SyntaxNodeAnalysisContext context )
+		{
+			if ( IsInterfaceMember( context.Node ) || IsExplicitInterfaceMember( context.Node ) )
+				return;
+
+			IDeclarationHandler handler = DeclarationHandlers.HandlerLookup[ context.Node.Kind() ];
+			SyntaxTokenList modifiers = handler.GetModifiers( context.Node );
+			if ( !modifiers.Any( m => IsVisibilityModifier( m ) ) )
+				context.ReportDiagnostic( Diagnostic.Create( _rule, context.Node.ChildTokens().First().GetLocation() ) );
+		}
+
+		private static bool IsInterfaceMember( SyntaxNode node ) => node.Ancestors().Any( n => n.IsKind( SyntaxKind.InterfaceDeclaration ) );
+		private static bool IsExplicitInterfaceMember( SyntaxNode node ) => node.DescendantNodes().Any( n => n.IsKind( SyntaxKind.ExplicitInterfaceSpecifier ) );
+
+		private static bool IsVisibilityModifier( SyntaxToken token ) =>
+			token.IsKind( SyntaxKind.PublicKeyword ) ||
+			token.IsKind( SyntaxKind.InternalKeyword ) ||
+			token.IsKind( SyntaxKind.PrivateKeyword ) ||
+			token.IsKind( SyntaxKind.ProtectedKeyword );
+	}
+}
---
+++ src/HellBrick.Diagnostics/AccessModifiers/AccessModifierCodeFixProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Composition;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+
+namespace HellBrick.Diagnostics.AccessModifiers
+{
+	[ExportCodeFixProvider( LanguageNames.CSharp, Name = nameof( AccessModifierCodeFixProvider ) ), Shared]
+	public class AccessModifierCodeFixProvider : CodeFixProvider
+	{
+		public sealed override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create( AccessModifierAnalyzer.DiagnosticID );
+		public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+		public sealed override async Task RegisterCodeFixesAsync( CodeFixContext context )
+		{
+			SyntaxNode root = await context.Document.GetSyntaxRootAsync( context.CancellationToken ).ConfigureAwait( false );
+			SyntaxNode node = root.FindNode( context.Span );
+			bool isClassMember = node.Ancestors().Any( n => n.IsKind( SyntaxKind.ClassDeclaration ) );
+			SyntaxToken missingKeyword = SyntaxFactory.Token( isClassMember ? SyntaxKind.PrivateKeyword : SyntaxKind.InternalKeyword );
+			IDeclarationHandler handler = DeclarationHandlers.HandlerLookup[ node.Kind() ];
+			SyntaxTokenList oldModifiers = handler.GetModifiers( node );
+			SyntaxTokenList newModifiers = oldModifiers.Insert( 0, missingKeyword );
+			SyntaxNode newNode = handler.WithModifiers( node, newModifiers );
+			newNode = newNode.WithLeadingTrivia( node.GetLeadingTrivia() );
+			SyntaxNode newRoot = root.ReplaceNode( node, newNode );
+			Document newDocument = context.Document.WithSyntaxRoot( newRoot );
+
+			CodeAction codeFix = CodeAction.Create( "Add missing access modifier", c => Task.FromResult( newDocument ), nameof( AccessModifierCodeFixProvider ) );
+			context.RegisterCodeFix( codeFix, context.Diagnostics[ 0 ] );
+		}
+	}
+}
