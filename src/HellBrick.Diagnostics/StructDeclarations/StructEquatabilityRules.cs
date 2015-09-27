db53adbd3573a2ec53dcf3db54fffc2f05292467
HellBrick <hellbrick@gmail.com>
2015-09-27 13:11:10 +0300

    Added struct equatibility infrastructure

---
+++ src/HellBrick.Diagnostics/StructDeclarations/StructEquatabilityRules.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HellBrick.Diagnostics.StructDeclarations.EquatabilityRules;
+using Microsoft.CodeAnalysis;
+
+namespace HellBrick.Diagnostics.StructDeclarations
+{
+	internal static class StructEquatabilityRules
+	{
+		public static ImmutableDictionary<string, IEquatabilityRule> Rules { get; } =
+			new IEquatabilityRule[]
+			{
+			}
+			.ToImmutableDictionary( rule => rule.ID );
+
+		public static ImmutableDictionary<string, DiagnosticDescriptor> Descriptors { get; } =
+			Rules.Values
+				.Select( rule => new DiagnosticDescriptor( rule.ID, "Structs " + rule.RuleText, "{0} " + rule.RuleText, DiagnosticCategory.Design, DiagnosticSeverity.Warning, true ) )
+				.ToImmutableDictionary( descriptor => descriptor.Id );
+	}
+}
