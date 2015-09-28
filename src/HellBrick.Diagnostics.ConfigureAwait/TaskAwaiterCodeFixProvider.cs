2a13fea32d23bf32bb711a7a1d2f534c9ae31272
HellBrick <hellbrick@gmail.com>
2015-09-28 20:27:16 +0300

    Refactored TaskAwaiter code fix to compute the document only when it's requested

--- src/HellBrick.Diagnostics.ConfigureAwait/TaskAwaiterCodeFixProvider.cs
+++ src/HellBrick.Diagnostics.ConfigureAwait/TaskAwaiterCodeFixProvider.cs
@@ -13,6 +13,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Rename;
 using Microsoft.CodeAnalysis.Text;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using HellBrick.Diagnostics.Utils;
 
 namespace HellBrick.Diagnostics.ConfigureAwait
 {
@@ -36,7 +37,14 @@ namespace HellBrick.Diagnostics.ConfigureAwait
 		public sealed override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create( TaskAwaiterAnalyzer.DiagnosticID );
 		public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
 
-		public sealed override async Task RegisterCodeFixesAsync( CodeFixContext context )
+		public sealed override Task RegisterCodeFixesAsync( CodeFixContext context )
+		{
+			CodeAction codeFix = CodeAction.Create( "Add 'ConfigureAwait'", c => AddConfigureAwaitAsync( context, c ), nameof( TaskAwaiterCodeFixProvider ) );
+			context.RegisterCodeFix( codeFix, context.Diagnostics[ 0 ] );
+			return TaskHelper.CompletedTask;
+		}
+
+		private async Task<Document> AddConfigureAwaitAsync( CodeFixContext context, CancellationToken cancellationToken )
 		{
 			SyntaxNode root = await context.Document.GetSyntaxRootAsync( context.CancellationToken ).ConfigureAwait( false );
 			ExpressionSyntax awaitedExpression = root.FindNode( context.Span ) as ExpressionSyntax;
@@ -54,9 +62,7 @@ namespace HellBrick.Diagnostics.ConfigureAwait
 				);
 
 			SyntaxNode newRoot = root.ReplaceNode( awaitedExpression, configureAwaitInvocation );
-			Document newDocument = context.Document.WithSyntaxRoot( newRoot );
-			CodeAction codeFix = CodeAction.Create( "Add 'ConfigureAwait'", c => Task.FromResult( newDocument ), nameof( TaskAwaiterCodeFixProvider ) );
-			context.RegisterCodeFix( codeFix, context.Diagnostics[ 0 ] );
+			return context.Document.WithSyntaxRoot( newRoot );
 		}
 	}
 }
