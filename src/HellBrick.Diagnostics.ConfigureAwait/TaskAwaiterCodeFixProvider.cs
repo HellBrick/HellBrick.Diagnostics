29e3ed9836674d1b5fb3a458ad3f1161479edfad
HellBrick <hellbrick@gmail.com>
2015-09-28 20:37:15 +0300

    Refactored TaskAwaiter code fix to allow generating code fix for both true and false arguments

--- src/HellBrick.Diagnostics.ConfigureAwait/TaskAwaiterCodeFixProvider.cs
+++ src/HellBrick.Diagnostics.ConfigureAwait/TaskAwaiterCodeFixProvider.cs
@@ -20,46 +20,36 @@ namespace HellBrick.Diagnostics.ConfigureAwait
 	[ExportCodeFixProvider( LanguageNames.CSharp, Name = nameof( TaskAwaiterCodeFixProvider ) ), Shared]
 	public class TaskAwaiterCodeFixProvider : CodeFixProvider
 	{
-		private static readonly ArgumentListSyntax _configureAwaitArgList =
-			ArgumentList
-			(
-				Token( SyntaxKind.OpenParenToken ),
-				SeparatedList
-				(
-					new ArgumentSyntax[]
-					{
-						Argument( LiteralExpression( SyntaxKind.FalseLiteralExpression ) )
-					}
-				),
-				Token( SyntaxKind.CloseParenToken )
-			);
+		private static readonly ArgumentSyntax _falseArgument = Argument( LiteralExpression( SyntaxKind.FalseLiteralExpression ) );
+		private static readonly ArgumentSyntax _trueArgument = Argument( LiteralExpression( SyntaxKind.TrueLiteralExpression ) );
 
 		public sealed override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create( TaskAwaiterAnalyzer.DiagnosticID );
 		public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
 
 		public sealed override Task RegisterCodeFixesAsync( CodeFixContext context )
 		{
-			CodeAction codeFix = CodeAction.Create( "Add 'ConfigureAwait'", c => AddConfigureAwaitAsync( context, c ), nameof( TaskAwaiterCodeFixProvider ) );
-			context.RegisterCodeFix( codeFix, context.Diagnostics[ 0 ] );
+			RegisterConfigureAwaitCodeFix( context, false );
 			return TaskHelper.CompletedTask;
 		}
 
-		private async Task<Document> AddConfigureAwaitAsync( CodeFixContext context, CancellationToken cancellationToken )
+		private void RegisterConfigureAwaitCodeFix( CodeFixContext context, bool captureContext )
+		{
+			CodeAction codeFix = CodeAction.Create
+			(
+				$"Add 'ConfigureAwait( {captureContext} )'",
+				c => AddConfigureAwaitAsync( context, captureContext, c ),
+				$"{nameof( TaskAwaiterCodeFixProvider )}.{captureContext}"
+			);
+			context.RegisterCodeFix( codeFix, context.Diagnostics[ 0 ] );
+		}
+
+		private async Task<Document> AddConfigureAwaitAsync( CodeFixContext context, bool captureContext, CancellationToken cancellationToken )
 		{
 			SyntaxNode root = await context.Document.GetSyntaxRootAsync( context.CancellationToken ).ConfigureAwait( false );
 			ExpressionSyntax awaitedExpression = root.FindNode( context.Span ) as ExpressionSyntax;
-
-			InvocationExpressionSyntax configureAwaitInvocation =
-				InvocationExpression
-				(
-					MemberAccessExpression
-					(
-						SyntaxKind.SimpleMemberAccessExpression,
-						awaitedExpression,
-						IdentifierName( "ConfigureAwait" )
-					),
-					_configureAwaitArgList
-				);
+			MemberAccessExpressionSyntax configureAwaitMember = MemberAccessExpression( SyntaxKind.SimpleMemberAccessExpression, awaitedExpression, IdentifierName( "ConfigureAwait" ) );
+			ArgumentSyntax argument = captureContext ? _trueArgument : _falseArgument;
+			InvocationExpressionSyntax configureAwaitInvocation = InvocationExpression( configureAwaitMember ).AddArgumentListArguments( argument );
 
 			SyntaxNode newRoot = root.ReplaceNode( awaitedExpression, configureAwaitInvocation );
 			return context.Document.WithSyntaxRoot( newRoot );
