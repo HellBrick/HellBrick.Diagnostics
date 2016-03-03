dc88ec480bea4bf6fc17fadc1e2d8bbd89ad33af
HellBrick <hellbrick@gmail.com>
2016-03-03 22:11:40 +0300

    Refactored analyzer to store state in a per-compilation class instance

--- src/HellBrick.Diagnostics/DeadCode/UnusedSymbolAnalyzer.cs
+++ src/HellBrick.Diagnostics/DeadCode/UnusedSymbolAnalyzer.cs
@@ -16,7 +16,6 @@ namespace HellBrick.Diagnostics.DeadCode
 	public class UnusedSymbolAnalyzer : DiagnosticAnalyzer
 	{
 		public const string DiagnosticID = IDPrefix.Value + "UnusedSymbol";
-		private static ConcurrentDictionary<string, HashSet<ISymbol>> _compilationSymbols = new ConcurrentDictionary<string, HashSet<ISymbol>>();
 
 		private static readonly DiagnosticDescriptor _rule = new DiagnosticDescriptor
 		(
@@ -34,97 +33,85 @@ namespace HellBrick.Diagnostics.DeadCode
 
 		private void StartAnalysis( CompilationStartAnalysisContext context )
 		{
-			if ( context.Compilation.AssemblyName == null )
-				return;
-
-			_compilationSymbols.AddOrUpdate( context.Compilation.AssemblyName, new HashSet<ISymbol>(), ( key, old ) => new HashSet<ISymbol>() );
-			context.RegisterSymbolAction( MarkDeclaredSymbols, ImmutableArray.Create( SymbolKind.Event, SymbolKind.Field, SymbolKind.Method, SymbolKind.Property ) );
-			context.RegisterCompilationEndAction( AnalyzeUnusedSymbols );
+			UnusedSymbolAnalysisContext analysisContext = new UnusedSymbolAnalysisContext();
+			context.RegisterSymbolAction( symbolContext => analysisContext.MarkDeclaredSymbols( symbolContext ), ImmutableArray.Create( SymbolKind.Event, SymbolKind.Field, SymbolKind.Method, SymbolKind.Property ) );
+			context.RegisterCompilationEndAction( compilationContext => analysisContext.AnalyzeUnusedSymbols( compilationContext ) );
 		}
 
-		private void MarkDeclaredSymbols( SymbolAnalysisContext context )
+		private class UnusedSymbolAnalysisContext
 		{
-			if ( context.Compilation.AssemblyName == null )
-				return;
+			private HashSet<ISymbol> _symbols = new HashSet<ISymbol>();
 
-			HashSet<ISymbol> symbols;
-			if ( !_compilationSymbols.TryGetValue( context.Compilation.AssemblyName, out symbols ) )
-				throw new InvalidOperationException( $"There's no symbol storage for {context.Compilation.AssemblyName}" );
-
-			if ( IsCandidate( context.Symbol ) )
-				symbols.Add( context.Symbol );
-		}
-
-		private static bool IsCandidate( ISymbol symbol ) =>
-			( symbol.DeclaredAccessibility == Accessibility.Private || symbol.DeclaredAccessibility == Accessibility.Internal ) &&
-			!symbol.IsOverride &&
-			!IsIgnoredMethod( symbol ) &&
-			!ImplementsInterface( symbol );
-
-		private static bool ImplementsInterface( ISymbol symbol ) =>
-			(
-				from @interface in symbol.ContainingType.AllInterfaces
-				from interfaceMember in @interface.GetMembers()
-				let implementation = symbol.ContainingType.FindImplementationForInterfaceMember( interfaceMember )
-				where symbol == implementation
-				select 0
-			)
-			.Any();
-
-		private static bool IsIgnoredMethod( ISymbol symbol )
-		{
-			IMethodSymbol methodSymbol = symbol as IMethodSymbol;
-			return
-				methodSymbol != null &&
-				(
-					methodSymbol.MethodKind == MethodKind.PropertyGet ||
-					methodSymbol.MethodKind == MethodKind.PropertySet ||
-					methodSymbol.IsOverride ||
-					methodSymbol.MetadataName == ".cctor"
-				);
-		}
-
-		private void AnalyzeUnusedSymbols( CompilationAnalysisContext context )
-		{
-			if ( context.Compilation.AssemblyName == null )
-				return;
-
-			HashSet<ISymbol> candidates;
-			if ( !_compilationSymbols.TryRemove( context.Compilation.AssemblyName, out candidates ) )
-				return;
-
-			foreach ( SyntaxTree syntaxTree in context.Compilation.SyntaxTrees )
+			public void MarkDeclaredSymbols( SymbolAnalysisContext context )
 			{
-				ReferencedSymbolDiscarder discarder = new ReferencedSymbolDiscarder( context.Compilation.GetSemanticModel( syntaxTree ), candidates );
-				discarder.Visit( syntaxTree.GetRoot() );
+				if ( IsCandidate( context.Symbol ) )
+					_symbols.Add( context.Symbol );
 			}
 
-			foreach ( ISymbol unusedSymbol in candidates )
+			private static bool IsCandidate( ISymbol symbol ) =>
+				( symbol.DeclaredAccessibility == Accessibility.Private || symbol.DeclaredAccessibility == Accessibility.Internal ) &&
+				!symbol.IsOverride &&
+				!IsIgnoredMethod( symbol ) &&
+				!ImplementsInterface( symbol );
+
+			private static bool ImplementsInterface( ISymbol symbol ) =>
+				(
+					from @interface in symbol.ContainingType.AllInterfaces
+					from interfaceMember in @interface.GetMembers()
+					let implementation = symbol.ContainingType.FindImplementationForInterfaceMember( interfaceMember )
+					where symbol == implementation
+					select 0
+				)
+				.Any();
+
+			private static bool IsIgnoredMethod( ISymbol symbol )
 			{
-				ISymbol definition = unusedSymbol.OriginalDefinition;
-				foreach ( Location symbolLocation in definition.Locations )
+				IMethodSymbol methodSymbol = symbol as IMethodSymbol;
+				return
+					methodSymbol != null &&
+					(
+						methodSymbol.MethodKind == MethodKind.PropertyGet ||
+						methodSymbol.MethodKind == MethodKind.PropertySet ||
+						methodSymbol.IsOverride ||
+						methodSymbol.MetadataName == ".cctor"
+					);
+			}
+
+			public void AnalyzeUnusedSymbols( CompilationAnalysisContext context )
+			{
+				foreach ( SyntaxTree syntaxTree in context.Compilation.SyntaxTrees )
 				{
-					Location diagnosticLocation = GetDiagnosticLocation( symbolLocation );
-					Diagnostic diagnostic = Diagnostic.Create( _rule, diagnosticLocation, unusedSymbol.ToString() );
-					context.ReportDiagnostic( diagnostic );
+					ReferencedSymbolDiscarder discarder = new ReferencedSymbolDiscarder( context.Compilation.GetSemanticModel( syntaxTree ), _symbols );
+					discarder.Visit( syntaxTree.GetRoot() );
+				}
+
+				foreach ( ISymbol unusedSymbol in _symbols )
+				{
+					ISymbol definition = unusedSymbol.OriginalDefinition;
+					foreach ( Location symbolLocation in definition.Locations )
+					{
+						Location diagnosticLocation = GetDiagnosticLocation( symbolLocation );
+						Diagnostic diagnostic = Diagnostic.Create( _rule, diagnosticLocation, unusedSymbol.ToString() );
+						context.ReportDiagnostic( diagnostic );
+					}
 				}
 			}
-		}
 
-		private static Location GetDiagnosticLocation( Location symbolLocation )
-		{
-			SyntaxNode root = symbolLocation.SourceTree.GetRoot();
-			SyntaxNode definitionNode = root.FindNode( symbolLocation.SourceSpan );
-			Location diagnosticLocation = definitionNode.GetLocation();
-
-			if ( definitionNode.HasStructuredTrivia && definitionNode.HasLeadingTrivia )
+			private static Location GetDiagnosticLocation( Location symbolLocation )
 			{
-				SyntaxTrivia leadingTrivia = definitionNode.GetLeadingTrivia().FirstOrDefault( t => t.IsKind( SyntaxKind.SingleLineDocumentationCommentTrivia ) );
-				if ( leadingTrivia != default( SyntaxTrivia ) )
-					diagnosticLocation = Location.Create( symbolLocation.SourceTree, TextSpan.FromBounds( leadingTrivia.FullSpan.Start, diagnosticLocation.SourceSpan.End ) );
-			}
+				SyntaxNode root = symbolLocation.SourceTree.GetRoot();
+				SyntaxNode definitionNode = root.FindNode( symbolLocation.SourceSpan );
+				Location diagnosticLocation = definitionNode.GetLocation();
 
-			return diagnosticLocation;
+				if ( definitionNode.HasStructuredTrivia && definitionNode.HasLeadingTrivia )
+				{
+					SyntaxTrivia leadingTrivia = definitionNode.GetLeadingTrivia().FirstOrDefault( t => t.IsKind( SyntaxKind.SingleLineDocumentationCommentTrivia ) );
+					if ( leadingTrivia != default( SyntaxTrivia ) )
+						diagnosticLocation = Location.Create( symbolLocation.SourceTree, TextSpan.FromBounds( leadingTrivia.FullSpan.Start, diagnosticLocation.SourceSpan.End ) );
+				}
+
+				return diagnosticLocation;
+			}
 		}
 	}
 }
