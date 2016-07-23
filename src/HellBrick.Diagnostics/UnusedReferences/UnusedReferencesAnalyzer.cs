75bd705ebcb59a88610ae158ed4295f2092cb3f8
HellBrick <hellbrick@outlook.com>
2016-07-23 10:33:37 +0300

    Migrated to ConfigureGeneratedCodeAnalysis( None ) (#57)

--- src/HellBrick.Diagnostics/UnusedReferences/UnusedReferencesAnalyzer.cs
+++ src/HellBrick.Diagnostics/UnusedReferences/UnusedReferencesAnalyzer.cs
@@ -22,6 +22,7 @@ namespace HellBrick.Diagnostics.UnusedReferences
 
 		public override void Initialize( AnalysisContext context )
 		{
+			context.ConfigureGeneratedCodeAnalysis( GeneratedCodeAnalysisFlags.None );
 			context.RegisterCompilationAction( FindUnusedReferences );
 		}
 
