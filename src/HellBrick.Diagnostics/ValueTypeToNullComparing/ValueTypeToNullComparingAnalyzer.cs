a3adcd6ad026da58ec51d13aeb4ff21a83485cbe
daniil <daniil@webkontrol.ru>
2016-12-02 23:04:51 +0300

    Implement ValueTypeToNullComparingAnalyzer

---
+++ src/HellBrick.Diagnostics/ValueTypeToNullComparing/ValueTypeToNullComparingAnalyzer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace HellBrick.Diagnostics.ValueTypeToNullComparing
+{
+	[DiagnosticAnalyzer( LanguageNames.CSharp )]
+	public class ValueTypeToNullComparingAnalyzer : DiagnosticAnalyzer
+	{
+		public const string DiagnosticId = IDPrefix.Value + "ValueTypeToNullComparing";
+		private const string _title = "Value type to null comparing";
+
+		private static readonly DiagnosticDescriptor _rule = new DiagnosticDescriptor
+		(
+			id: DiagnosticId,
+			title: _title,
+			messageFormat: _title,
+			category: DiagnosticCategory.Design,
+			defaultSeverity: DiagnosticSeverity.Warning,
+			isEnabledByDefault: true
+		);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create( _rule );
+
+		public override void Initialize( AnalysisContext context )
+		{
+			context.ConfigureGeneratedCodeAnalysis( GeneratedCodeAnalysisFlags.None );
+			context.RegisterSyntaxNodeAction( FindFlagrantViolationOfCodeStyle, SyntaxKind.EqualsExpression, SyntaxKind.NotEqualsExpression );
+		}
+
+		private void FindFlagrantViolationOfCodeStyle( SyntaxNodeAnalysisContext context )
+		{
+			BinaryExpressionSyntax typedNode = (BinaryExpressionSyntax) context.Node;
+
+			bool shouldReportDiagnostic =
+				NodesAreNullAndValueType( typedNode.Right, typedNode.Left, context.SemanticModel ) ||
+				NodesAreNullAndValueType( typedNode.Left, typedNode.Right, context.SemanticModel );
+
+			if ( shouldReportDiagnostic )
+			{
+				Diagnostic diagnostic = Diagnostic.Create( _rule, context.Node.GetLocation() );
+				context.ReportDiagnostic( diagnostic );
+			}
+		}
+
+		private static bool NodesAreNullAndValueType( ExpressionSyntax left, ExpressionSyntax right, SemanticModel semanticModel )
+			=> left.IsKind( SyntaxKind.NullLiteralExpression )
+			&& semanticModel.GetTypeInfo( right ).Type.TypeKind == TypeKind.Struct;
+	}
+}
---
+++ src/HellBrick.Diagnostics/ValueTypeToNullComparing/ValueTypeToNullComparingCodeFixProvider.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HellBrick.Diagnostics.Utils;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace HellBrick.Diagnostics.ValueTypeToNullComparing
+{
+	[ExportCodeFixProvider( LanguageNames.CSharp, Name = nameof( ValueTypeToNullComparingCodeFixProvider ) ), Shared]
+	public class ValueTypeToNullComparingCodeFixProvider : CodeFixProvider
+	{
+		private const string _title = "Replace with `default` statement";
+
+		public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create( ValueTypeToNullComparingAnalyzer.DiagnosticId );
+		public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+		public sealed override Task RegisterCodeFixesAsync( CodeFixContext context )
+		{
+			Diagnostic diagnostic = context.Diagnostics.First();
+
+			context.RegisterCodeFix
+			(
+				CodeAction.Create
+				(
+					title: _title,
+					createChangedDocument: c => ReplaceNullWithDefalutSyntax( context.Document, diagnostic, c ),
+					equivalenceKey: _title
+				),
+					diagnostic
+			);
+			return TaskHelper.CompletedTask;
+		}
+
+		private async Task<Document> ReplaceNullWithDefalutSyntax( Document document, Diagnostic diagnostic, CancellationToken token )
+		{
+			SyntaxNode documentRoot = await document
+				.GetSyntaxRootAsync( token )
+				.ConfigureAwait( false );
+
+			SemanticModel model = await document
+				.GetSemanticModelAsync().ConfigureAwait( false );
+
+			BinaryExpressionSyntax equalStatement = documentRoot
+				.FindNode( diagnostic.Location.SourceSpan ) as BinaryExpressionSyntax;
+
+			BinaryExpressionSyntax newExpression = equalStatement.Right.IsKind( SyntaxKind.NullLiteralExpression )
+				? equalStatement.WithRight( CreateDefaultSyntax( equalStatement.Left, model, diagnostic.Location ) )
+				: equalStatement.WithLeft( CreateDefaultSyntax( equalStatement.Right, model, diagnostic.Location ) );
+
+			return document.WithSyntaxRoot( documentRoot.ReplaceNode( equalStatement, newExpression ) );
+		}
+
+		private static DefaultExpressionSyntax CreateDefaultSyntax( ExpressionSyntax node, SemanticModel model, Location location )
+		{
+			string typeName = model.GetTypeInfo( node ).Type.ToMinimalDisplayString( model, location.SourceSpan.Start );
+			return SyntaxFactory.DefaultExpression( SyntaxFactory.ParseTypeName( typeName ) );
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
