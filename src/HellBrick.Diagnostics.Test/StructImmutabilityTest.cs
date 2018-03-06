dcf929f2fbad0b46672b103bb9b1f5fe5e53de90
HellBrick <hellbrick@outlook.com>
2018-03-06 12:34:22 +0300

    Removed unused usings

--- src/HellBrick.Diagnostics.Test/StructImmutabilityTest.cs
+++ src/HellBrick.Diagnostics.Test/StructImmutabilityTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HellBrick.Diagnostics.StructDeclarations;
+﻿using HellBrick.Diagnostics.StructDeclarations;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using TestHelper;
