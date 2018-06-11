43f0bc95d5cef2bc1a06094ed451a2d2a9f6cbbc
HellBrick <hellbrick@outlook.com>
2018-06-11 13:05:28 +0300

    Added CSharpCodeStyleOptions.UseImplicitTypeForIntrinsicTypes

---
+++ src/HellBrick.Diagnostics/Utils/CSharpCodeStyleOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.CodeAnalysis.CodeStyle;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
+using Microsoft.CodeAnalysis.Options;
+
+namespace HellBrick.Diagnostics.Utils
+{
+	public static class CSharpCodeStyleOptions
+	{
+		private static readonly Type _csharpCodeStyleOptionsType
+			= typeof( CSharpFormattingOptions )
+			.GetTypeInfo()
+			.Assembly
+			.GetType( "Microsoft.CodeAnalysis.CSharp.CodeStyle.CSharpCodeStyleOptions" );
+
+		public static Option<CodeStyleOption<bool>> UseImplicitTypeForIntrinsicTypes { get; }
+			= _csharpCodeStyleOptionsType
+			.GetRuntimeField( nameof( UseImplicitTypeForIntrinsicTypes ) )
+			.GetValue( null ) as Option<CodeStyleOption<bool>>;
+	}
+}
