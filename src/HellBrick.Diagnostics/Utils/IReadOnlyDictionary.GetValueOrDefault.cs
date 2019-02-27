b34bcf5c2678054ccbde47168d9aee06f24beeba
HellBrick <hellbrick@outlook.com>
2019-02-27 21:15:58 +0300

    Added IReadOnlyDictionary.GetValueOrDefault()

---
+++ src/HellBrick.Diagnostics/Utils/IReadOnlyDictionary.GetValueOrDefault.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace HellBrick.Diagnostics.Utils
+{
+	public static class DictionaryExtensions
+	{
+		public static TValue GetValueOrDefault<TKey, TValue>( this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key )
+			=> dictionary.TryGetValue( key, out TValue value )
+				? value
+				: default;
+	}
+}
