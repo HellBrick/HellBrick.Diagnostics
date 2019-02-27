681f30c46ce0bce2e005ba3b6b753b18364b9eb1
HellBrick <hellbrick@outlook.com>
2019-02-27 21:45:40 +0300

    Added IDictionary.GetOrAdd()

---
+++ src/HellBrick.Diagnostics/Utils/IDictionary.GetOrAdd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HellBrick.Diagnostics.Utils
+{
+	public static partial class DictionaryExtensions
+	{
+		public static TValue GetOrAdd<TKey, TValue>( this IDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> valueFactory )
+		{
+			if ( dictionary.TryGetValue( key, out TValue value ) )
+			{
+				return value;
+			}
+			else
+			{
+				TValue newValue = valueFactory( key );
+				dictionary.Add( key, newValue );
+				return newValue;
+			}
+		}
+	}
+}
