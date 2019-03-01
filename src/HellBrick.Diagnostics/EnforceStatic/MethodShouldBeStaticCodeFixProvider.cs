8dbaa54550ed85187fc9662e0c4cf92e9c190149
HellBrick <hellbrick@outlook.com>
2019-03-01 14:12:09 +0300

    Switched MethodShouldBeStatic code fix to IChange + ApplyChanges

--- src/HellBrick.Diagnostics/EnforceStatic/MethodShouldBeStaticCodeFixProvider.cs
+++ src/HellBrick.Diagnostics/EnforceStatic/MethodShouldBeStaticCodeFixProvider.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using HellBrick.Diagnostics.Utils;
+using HellBrick.Diagnostics.Utils.MultiChanges;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
@@ -27,52 +29,75 @@ namespace HellBrick.Diagnostics.EnforceStatic
 
 		public override Task RegisterCodeFixesAsync( CodeFixContext context )
 		{
-			CodeAction codeFix = CodeAction.Create( "Make static", ct => UpdateDocumentAsync( context, ct ) );
+			CodeAction codeFix = CodeAction.Create( "Make static", ct => UpdateSolutionAsync( context, ct ) );
 			context.RegisterCodeFix( codeFix, context.Diagnostics[ 0 ] );
 			return Task.CompletedTask;
 		}
 
-		private static async Task<Document> UpdateDocumentAsync( CodeFixContext context, CancellationToken cancellationToken )
+		private static async Task<Solution> UpdateSolutionAsync( CodeFixContext context, CancellationToken cancellationToken )
 		{
+			Solution solution = context.Document.Project.Solution;
 			SyntaxNode root = await context.Document.GetSyntaxRootAsync( cancellationToken ).ConfigureAwait( false );
 			DocumentOptionSet options = await context.Document.GetOptionsAsync( cancellationToken ).ConfigureAwait( false );
 
 			MethodDeclarationSyntax oldDeclaration = (MethodDeclarationSyntax) root.FindNode( context.Span );
-			SyntaxTokenList modifierList = oldDeclaration.Modifiers;
 
-			CodeStyleOption<string> option = options.GetOption( CSharpCodeStyleOptions.PreferredModifierOrder );
+			IChange declarationChange = new DeclarationChange( oldDeclaration, options );
+			IEnumerable<IChange> allChanges = new[] { declarationChange };
 
-			string[] orderedModifiers
-				= option
-				.Value
-				.Split( _preferredModifierSeparator, StringSplitOptions.RemoveEmptyEntries )
-				.Select( modifier => modifier.Trim() )
-				.ToArray();
+			return solution.ApplyChanges( allChanges, cancellationToken );
+		}
 
-			int staticOrder = Array.IndexOf( orderedModifiers, "static" );
+		private class DeclarationChange : IChange
+		{
+			private readonly MethodDeclarationSyntax _methodDeclaration;
+			private readonly DocumentOptionSet _options;
 
-			int indexOfLastModifierThatShouldPrecedeStatic
-				= modifierList
-				.Select( ( modifierToken, index ) => (modifierToken, index) )
-				.Aggregate
-				(
-					-1,
-					( previousIndex, pair )
-						=> Array.IndexOf( orderedModifiers, pair.modifierToken.Text ) < staticOrder
-							? pair.index
-							: previousIndex
-				);
+			public DeclarationChange( MethodDeclarationSyntax methodDeclaration, DocumentOptionSet options )
+			{
+				_methodDeclaration = methodDeclaration;
+				_options = options;
+			}
 
-			MethodDeclarationSyntax newDeclaration
-				= oldDeclaration
-				.WithModifiers
-				(
-					oldDeclaration.Modifiers.Insert( indexOfLastModifierThatShouldPrecedeStatic + 1, Token( SyntaxKind.StaticKeyword ) )
-				);
+			public SyntaxNode ReplacedNode => _methodDeclaration;
 
-			SyntaxNode newRoot = root.ReplaceNode( oldDeclaration, newDeclaration );
-			Document newDocument = context.Document.WithSyntaxRoot( newRoot );
-			return newDocument;
+			public SyntaxNode ComputeReplacementNode( SyntaxNode replacedNode )
+			{
+				MethodDeclarationSyntax oldDeclaration = (MethodDeclarationSyntax) replacedNode;
+				SyntaxTokenList modifierList = oldDeclaration.Modifiers;
+
+				CodeStyleOption<string> option = _options.GetOption( CSharpCodeStyleOptions.PreferredModifierOrder );
+
+				string[] orderedModifiers
+					= option
+					.Value
+					.Split( _preferredModifierSeparator, StringSplitOptions.RemoveEmptyEntries )
+					.Select( modifier => modifier.Trim() )
+					.ToArray();
+
+				int staticOrder = Array.IndexOf( orderedModifiers, "static" );
+
+				int indexOfLastModifierThatShouldPrecedeStatic
+					= modifierList
+					.Select( ( modifierToken, index ) => (modifierToken, index) )
+					.Aggregate
+					(
+						-1,
+						( previousIndex, pair )
+							=> Array.IndexOf( orderedModifiers, pair.modifierToken.Text ) < staticOrder
+								? pair.index
+								: previousIndex
+					);
+
+				MethodDeclarationSyntax newDeclaration
+					= oldDeclaration
+					.WithModifiers
+					(
+						oldDeclaration.Modifiers.Insert( indexOfLastModifierThatShouldPrecedeStatic + 1, Token( SyntaxKind.StaticKeyword ) )
+					);
+
+				return newDeclaration;
+			}
 		}
 	}
 }
